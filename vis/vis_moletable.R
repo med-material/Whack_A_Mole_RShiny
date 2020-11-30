b7cd5be74c2c03436d775b56d3f2a687e433abd9
Bastian Ilso <hougaard.junior@gmail.com>
2020-11-30 11:44:23 +0100

    vis_moletable.R: Visualize Mole Events
    Adds a table visualization which can show mole
    events. Alters our dataframe validation to
    instead be an if statement.

---
+++ vis/vis_moletable.R
@@ -0,0 +1,19 @@
+library(dplyr)
+vis_moleTable <- function(df) {
+  moleEvents = c("Mole Spawned", "Fake Mole Spawned", "Pointer Shoot", "Mole Hit", "Mole Missed","Fake Mole Hit")
+  table = data.frame(moleEvents, rep(NA,length(moleEvents)))
+  names(table) = c("Event", "Count")
+  stats = df_tmp %>%
+    group_by(Event) %>% 
+    dplyr::summarise(Count = n()) %>%
+    filter(Event %in% moleEvents) %>%
+    mutate(Event = factor(Event, levels = moleEvents),
+            ) %>%
+    arrange(Event)
+  table <- table %>%
+    left_join(stats, by = "Event") %>%
+    mutate(Count = as.integer(Count.y),
+           Count.x = NULL,
+           Count.y = NULL)
+  return(table)
+}
