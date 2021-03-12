ab4626f97d59ab607da84268595dc3091afa7de3
Bastian Ilso <hougaard.junior@gmail.com>
2021-03-12 14:49:49 +0100

    data_selection_summary: use new Timestamp.Meta
    since the update of loaddbdata, our timestamps
    now get merged in a different way.

--- modules/data_selection_summary_module.R
+++ modules/data_selection_summary_module.R
@@ -7,15 +7,18 @@ data_selection_summary_UI <- function(id) {
 
 data_selection_summary <- function(input, output, session, df) {
   ns <- session$ns
-
+  
   output$data_selection_text <- renderUI({
+    validate(need(!is.na(df()), "Database not available."))
+    validate(need(!is.null(df()), "Loading dataset.."))
     HTML(paste(
+      "<small>",
       "Data from ",
-      wday(min(as.character(df()$MetaTimestamp), na.rm=T), abbr = F, label=T),
-      format(date(min(as.character(df()$MetaTimestamp), na.rm=T)), "%d %b %Y"),
-      sprintf("%02d:%02d", hour(min(as.character(df()$MetaTimestamp), na.rm=T)), minute(min(as.character(df()$MetaTimestamp), na.rm=T))),
-      "<br><small>",
-      as.character(min(as.character(df()$MetaEmail), na.rm=T)),
+      wday(min(as.character(df()$Timestamp.Meta), na.rm=T), abbr = F, label=T),
+      format(date(min(as.character(df()$Timestamp.Meta), na.rm=T)), "%d %b %Y"),
+      sprintf("%02d:%02d", hour(min(as.character(df()$Timestamp.Meta), na.rm=T)), minute(min(as.character(df()$Timestamp.Meta), na.rm=T))),
+      "</small><br><small>",
+      as.character(min(as.character(df()$Email.Meta), na.rm=T)),
       "(",
       str_sub(max(as.character(df()$SessionID), na.rm=T),-6,-1),
       ")",
