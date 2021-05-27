43387ff484f235b505f4bee44dfa8cd9880229c7
Bastian Ilso <contact@bastianilso.com>
2021-05-27 15:38:26 +0200

    ui.R: use navListPanel as sub list
    We are going to have many categories and
    they are too hard to explain in small
    words. Prepare the interface for new
    plots.

--- ui.R
+++ ui.R
@@ -24,17 +24,28 @@ shinyUI(fluidPage(
     )),
     navlistPanel(fluid= FALSE, widths=c(1,11), well = FALSE, id = "overall-nav",
         tabPanel(title = div(class="text-center", img(src='nav_individual.svg', style="max-width:100%;"),tags$br(),"Individual"),
-            #  Output ----------------
-            tabsetPanel(id = "analysisChooser", type = "tabs",
-                tabPanel(value = "Player Overview", id = "PlayerOverview", strong("Player Overview"), icon=icon('user'),
-                    div(class="main-content", player_overview_UI("overview_panel"))
-                ),
-                tabPanel(value  = "Time (X-axis)", id = "Timeline", strong("Timeline"), icon = icon('chart-bar'),
-                    div(class="main-content", game_timeline_UI("timeline_panel"))
-                ),
-                # Rest of Page ---------------------------------------------------------------
-                tags$footer()
-            )
+                 navlistPanel(id = "analysisChooser", well= FALSE, widths=c(2,8), fluid = FALSE,
+                   tabPanel(value = "Player Overview", id = "PlayerOverview", HTML("Player Overview<br><small>See overview information.</small>"),
+                            div(class="main-content", player_overview_UI("overview_panel"))
+                   ),
+                   tabPanel(value  = "Game Performance", id = "GamePerf", HTML("Game Performance<br><small>Successful hits, misses and speed.</small>"),
+                            div(class="main-content", tags$p("Under Construction.."))
+                   ),
+                   tabPanel(value  = "Head Movement", id = "HeadMove", HTML("Head Movement<br><small>Position and orientation of the head.</small>"),
+                            div(class="main-content", tags$p("Under Construction.."))
+                   ),
+                   tabPanel(value  = "Eye Movement", id = "EyeMove", HTML("Eye Movement<br><small>Hemispheric position and 3D gaze activity.</small>"),
+                            div(class="main-content", tags$p("Under Construction.."))
+                   ),
+                   tabPanel(value  = "Controller Movement", id = "ControllerMove", HTML("Controller Movement<br><small>Left and right controller activity.</small>"),
+                            div(class="main-content", tags$p("Under Construction.."))
+                   ),
+                   tabPanel(value  = "Time (X-axis)", id = "Timeline", HTML("Timeline<br><small>Replay the game from start to end.</small>"),
+                            div(class="main-content", game_timeline_UI("timeline_panel"))
+                   ),
+                    # Rest of Page ---------------------------------------------------------------
+                    tags$footer()
+                )
         ),
         tabPanel(title = div(class="text-center", img(src='nav_trends.svg', style="max-width:100%;"),tags$br(),"Trends"),
                  tabPanel(value = "Under Construction", id = "TrendsOverview", strong("Player Overview"), icon=icon('user'),
