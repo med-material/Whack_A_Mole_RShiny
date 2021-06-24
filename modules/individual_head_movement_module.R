cb86bfac3ea094ac41bd70f6a0b1ef3f9e61cd00
Bastian Ilso <contact@bastianilso.com>
2021-06-24 16:38:18 +0200

    individual_head: fix calcs and add density plot
    We were using GameTimeSpent but GameTimeSpent
    is only available in Event, not in Sample. use
    the timestamp instead.
    
    Secondly, add a density plot to provide spatial overview.

--- modules/individual_head_movement_module.R
+++ modules/individual_head_movement_module.R
@@ -8,29 +8,43 @@ individual_head_movement_UI <- function(id) {
       column(6, uiOutput(ns("head_ori_lr"))),
       column(6, uiOutput(ns("head_pos_lr")))
     ),
+    fluidRow(
+      tags$h3("Head Orientation Density", class="page-header")
+    ),
+    fluidRow(
+    plot_head_orientation_density_UI(ns("ori_density"))
+    )
   )
 }
 
 individual_head_movement <- function(input, output, session, df, meta) {
   ns <- session$ns
 
+  observe({
+    req(!is.na(df()))
+    req(!is.null(df()))
+    callModule(plot_head_orientation_density, "ori_density", df)
+  })
+
   output$head_ori_lr <- renderUI({
     req(!is.na(df()))
     req(!is.null(df()))
+
+    timestamp_start = df() %>% filter(Event == "Game Started") %>% select(Timestamp) %>% slice(1) %>% pull(1)
     
-    aggr = df() %>%
-      dplyr::mutate(HeadCameraRotEulerY_wrap = ifelse(HeadCameraRotEulerY > 180, HeadCameraRotEulerY - 360, HeadCameraRotEulerY),
-                    row_duration = GameTimeSpent - lag(GameTimeSpent),
-                    head_lr = "Center", 
+    df_h <- df() %>% filter(Timestamp > timestamp_start)
+    aggr = df_h %>%
+      dplyr::mutate(HeadCameraRotEulerY_wrap = ifelse(HeadCameraRotEulerY > 180, HeadCameraRotEulerY - 360, HeadCameraRotEulerY)) %>%
+                    drop_na(HeadCameraRotEulerY) %>%
+      dplyr::mutate(head_lr = "Center",
                     head_lr = ifelse(HeadCameraRotEulerY_wrap > 0, "Right", head_lr),
                     head_lr = ifelse(HeadCameraRotEulerY_wrap < 0, "Left", head_lr)) %>%
     group_by(head_lr) %>% 
-      dplyr::summarise(Sum = sum(row_duration)) %>%
-     pivot_wider(names_from = "head_lr", values_from="Sum")
+      dplyr::summarise(s = sum(row_duration, na.rm=T)) %>%
+      pivot_wider(names_from = "head_lr", values_from="s")
     
     fallback_text = "No data available."
     if (nrow(aggr) > 0) {
-      #browser()
       left_text = paste("<strong>Left Head Orientation:</strong>",
                                 sprintf("%.2f", aggr$Left),
                                  "seconds.")
@@ -65,15 +79,16 @@ individual_head_movement <- function(input, output, session, df, meta) {
     req(!is.na(df()))
     req(!is.null(df()))
     
-    aggr = df() %>%
-      dplyr::mutate(row_duration = GameTimeSpent - lag(GameTimeSpent),
-                    head_lr = "Center", 
+    timestamp_start = df() %>% filter(Event == "Game Started") %>% select(Timestamp) %>% slice(1) %>% pull(1)
+    df_h <- df() %>% filter(Timestamp > timestamp_start)
+    aggr = df_h %>%
+      drop_na(HeadCameraPosWorldX) %>%
+      dplyr::mutate(head_lr = "Center",
                     head_lr = ifelse(HeadCameraPosWorldX > 0, "Right", head_lr),
                     head_lr = ifelse(HeadCameraPosWorldX < 0, "Left", head_lr)) %>%
       group_by(head_lr) %>% 
-      dplyr::summarise(Sum = sum(row_duration)) %>%
+      dplyr::summarise(Sum = sum(row_duration, na.rm=T)) %>%
       pivot_wider(names_from = "head_lr", values_from="Sum")
-    
     fallback_text = "No data available."
     if (nrow(aggr)) {
       left_text = paste("<strong>Left Head Position:</strong>",
