8ce1038b0b2a34da5501a659321a9cd31460af55
Bastian Ilso <contact@bastianilso.com>
2024-07-03 18:00:07 +0200

    preprocess: add ControllerNameHit and missing cols
    1) Make the current controller hitting the mole
    available in the data. This is currently approximate
    since ControllerName has odd behavior.
    2) if columns are missing, add them as NA, to be
    more resilient to poor datasets.

--- utils/preprocessdata.R
+++ utils/preprocessdata.R
@@ -3,12 +3,17 @@ PreprocessGlobalData <- function(df) {
     dplyr::mutate(Timestamp = as.POSIXct(Timestamp.Event, format = "%Y-%m-%d %H:%M:%OS")) %>% 
     arrange(Timestamp) %>%
     dplyr::mutate(row_duration = as.numeric(difftime(Timestamp, lag(Timestamp)),units="secs"))
-  
+
+  # Introduce cols that unexpectedly don't exist, init them as NA.
+  cols = read.csv("wam_expected_cols.csv", sep=";") %>% select(-type)
+  cols = cols %>% filter(!name %in% colnames(D)) 
+  if (nrow(cols) > 0) { cols = cols %>% mutate(val = as.numeric(NA)) %>% pivot_wider(names_from = name, values_from = val) }
+  D = D %>% bind_cols(cols)
+    
   ####
   # HitOrder: Determine what constitutes each player action
   ####
   # we cant use mole expired in multi-mole scenarios, as a consequence we just get longer actions which is fine.
-  # todo: currently doesnt consider which controller hits what.
   
   # Create duplicate rows of Mole Hit and Mole Expired, but call them "Action Ended"
   D = D %>% dplyr::mutate(orderflag = 1)
@@ -28,15 +33,16 @@ PreprocessGlobalData <- function(df) {
   # consider only moles after the first hit, since we have clear start and end positions for them.
   #D = D %>% filter(HitOrder > 0)
   # Debug
-  #D %>% filter(Event %in% c("Mole Spawned","Mole Hit","Pointer Hover Begin","Pointer Hover End","Hit End","Hit Begin")) %>% select(Timestamp, Event,MoleId, HitOrder, PatternSegmentLabel) %>% view()
+  #D %>% filter(HitOrder == 1, Event %in% c("Mole Spawned","Sample", "Mole Hit","Pointer Hover Begin","Pointer Hover End","Hit End","Hit Begin")) %>% select(ControllerName,Timestamp, Event,MoleId, HitOrder, PatternSegmentLabel) %>% view()
   
   # Move MoleId from HitBegin to "MoleIdStart" and "MoleIdToHit"
   D = D %>% dplyr::group_by(HitOrder) %>% dplyr::mutate(
     MoleIdStart = ifelse(Event == "Hit Begin", MoleId,NA),
-    MoleIdToHit = ifelse(Event == "Hit End", MoleId, NA)
-  ) %>% tidyr::fill(MoleIdStart,MoleIdToHit, .direction="downup")
+    MoleIdToHit = ifelse(Event == "Hit End", MoleId, NA),
+    ControllerNameHit = ifelse(Event == "Hit End", ControllerName, NA)
+  ) %>% tidyr::fill(MoleIdStart,MoleIdToHit,ControllerNameHit, .direction="downup")
   # Debug
-  #D %>% filter(Event %in% c("Mole Spawned","Mole Hit","Pointer Hover Begin","Pointer Hover End","Hit End","Hit Begin")) %>% select(Timestamp, Event,MoleId,MoleIdStart,MoleIdToHit, HitOrder, PatternSegmentLabel) %>% view()
+  #D %>% filter(Event %in% c("Mole Spawned","Mole Hit","Pointer Hover Begin","Pointer Hover End","Hit End","Hit Begin")) %>% select(Timestamp, ControllerNameHit,Event,MoleId,MoleIdStart,MoleIdToHit, HitOrder, PatternSegmentLabel) %>% view()
   
   # 0: filter out the first HitOrder, since the users cursor has unknown starting point (no Hit Begin).
   # 1: flag the first action (mole to hit) after each break, so they can be filtered out.
@@ -77,6 +83,33 @@ PreprocessGlobalData <- function(df) {
                                 MoleStartPositionY < MoleEndPositionY ~ "Up",
                                 TRUE ~ "Horisontal")
     )
+  
+  ####
+  # Create columns mapping any controller currently hitting things.
+  ###
+  D = D %>% ungroup() %>% dplyr::mutate(
+    ControllerNameHit = ifelse(ControllerNameHit == "Controller (left)", "Left",ControllerNameHit),
+    ControllerNameHit = ifelse(ControllerNameHit == "Controller (right)", "Right",ControllerNameHit)
+  )
+  
+  # Fill down ControllerNameHit, since in some cases Mole Hit has no information about ControllerName
+  # instead it appears to come from Pointer Shoot?
+  D = D %>% tidyr::fill(ControllerNameHit, .direction="down")
+  
+  
+  D = D %>% dplyr::group_by(HitOrder) %>%
+    dplyr::mutate(
+      ControllerPosWorldX = case_when(ControllerNameHit == "Right" ~ RightControllerPosWorldX, 
+                                      ControllerNameHit == "Left" ~ LeftControllerPosWorldX),
+      ControllerPosWorldY = case_when(ControllerNameHit == "Right" ~ RightControllerPosWorldY, 
+                                      ControllerNameHit == "Left" ~ LeftControllerPosWorldY),
+      ControllerLaserPosWorldX = case_when(ControllerNameHit == "Right" ~ RightControllerLaserPosWorldX, 
+                                           ControllerNameHit == "Left" ~ LeftControllerLaserPosWorldX),
+      ControllerLaserPosWorldY = case_when(ControllerNameHit == "Right" ~ RightControllerLaserPosWorldY, 
+                                           ControllerNameHit == "Left" ~ LeftControllerLaserPosWorldY),
+    )
+  
+
   
   # important to ungroup everything once preprocessing is over.
   D = D %>% ungroup()
